2532d377e3171eec1ddf9e9d4a093f4b618b0475
vlifirenko <vladislav.lifirenko@gmail.com>
2024-08-07 17:06:11 +0300

    Setup

--- engine-3d-cpp.cpp
+++ engine-3d-cpp.cpp
@@ -1,20 +1,53 @@
-// engine-3d-cpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include "olcConsoleGameEngine.h"
+using namespace std;
 
-#include <iostream>
+struct vec3d
+{
+	float x, y, z;
+};
+
+struct triangle
+{
+	vec3d p[3];
+};
+
+struct mesh
+{
+	vector<triangle> tris;
+};
+
+class Engine3D : public olcConsoleGameEngine
+{
+public:
+	Engine3D()
+	{
+		m_sAppName = L"3D DEMO";
+	}
+
+private:
+	mesh meshCube;
+
+public:
+	bool OnUserCreate() override
+	{
+		meshCube.tris = {
+
+		};
+
+		return true;
+	}
+
+	bool OnUserUpdate(float fElapsedTime) override
+	{
+		return true;
+	}
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	Engine3D demo;
+	if (demo.ConstructConsole(256, 240, 4, 4))
+		demo.Start();
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
